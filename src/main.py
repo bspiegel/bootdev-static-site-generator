3de7e1a27914cffb139c90724a1f7b1136e6454e
Brian Spiegel <greyjello+github@gmail.com>
2025-10-16 10:50:31 -0400

    CH2: L3

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 def main():
     node = TextNode("jumpin' jehosephet!", TextType.BOLD)
     print(node)
+    html = HTMLNode("peanut", "butter")
+    html2 = HTMLNode("jelly", "sandwich", [html], {"prop1": "val1", "prop2": "val2"})
+    print(html2)
 
 
 if __name__ == "__main__":
