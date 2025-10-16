1460eefde4ac5b9a31aca855204869ea71a3af9d
Brian Spiegel <greyjello+github@gmail.com>
2025-10-16 11:25:00 -0400

    CH2: L5

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,3 +31,21 @@ class LeafNode(HTMLNode):
         if not self.tag:
             return f"{self.value}"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("parent node must have a tag")
+        if not self.children:
+            raise ValueError("parent tag must have children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 def main():
     node = TextNode("jumpin' jehosephet!", TextType.BOLD)
     print(node)
+
     html = HTMLNode("peanut", "butter")
     html2 = HTMLNode("jelly", "sandwich", [html], {"prop1": "val1", "prop2": "val2"})
     print(html2)
+
+    pnode = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+
+    print(pnode)
+    print(pnode.to_html())
 
 
 if __name__ == "__main__":
