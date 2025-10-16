1c85b24c3e6f91e3020c1c29ec4b094a561d47c3
Brian Spiegel <greyjello+github@gmail.com>
2025-10-16 11:09:17 -0400

    CH2: L4

---
+++ src/htmlnode.py
@@ -0,0 +1,33 @@
+class HTMLNode:
+
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html() method not implemented yet")
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        props_string = ""
+        for prop in self.props:
+            props_string += f' {prop}="{self.props[prop]}"'
+        return props_string
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("leaf nodes must have a value")
+        if not self.tag:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
